5279d894d7a56892de274af89f6fd66e8cf36fba
ilsafix <55927732+ilsafix@users.noreply.github.com>
2023-04-11 17:33:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,6 +14,6 @@ def solution(x_success: int,
     n1 = x_cnt
     n2 = y_cnt
     
-    z = abs((p1 - p2) / np.sqrt(p * (1 - p) * (1/n1 + 1/n2)))
+    z = np.abs((p1 - p2) / np.sqrt(p * (1 - p) * (1/n1 + 1/n2)))
     
     return z > 2.1701
